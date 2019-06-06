a6465698b411283e5fabbd83d7211f7f3c8fdee9
zara <zara.wang85@gmail.com>
2019-06-06 15:25:03 -0400

    pybank terminal part

---
+++ PyBank/main.py
@@ -0,0 +1,49 @@
+import os
+import csv
+csvpath = os.path.join('dudget_data.csv')
+
+with open(csvpath, newline='') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    budget = csv.reader(csvfile, delimiter=',')
+    csv_header = next(budget)
+    # print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+
+    monthscnt = 0
+    total = 0
+    profit_loss= []
+    month = []
+    change = []
+    max = ""
+    min = ""
+    totalchange = 0
+       
+    for row in budget:
+        monthscnt = monthscnt +1
+        total = total + int(row[1])
+        month.append(row[0])
+        profit_loss.append(row[1])
+    month.pop(0)
+    for i in range(len(profit_loss)-1):
+        change.append(int(profit_loss[i+1])-int(profit_loss[i]))
+   
+    increase = change[0]
+    decrease = change[0]
+    for mon,cha in zip(month,change):
+        if cha >= increase:
+           increase = cha
+           max = mon  
+        if cha <= decrease:
+           decrease = cha
+           min = mon
+        totalchange = totalchange + cha
+print("Financial Analysis")
+print("-------------------------------------------------------------------")
+print("Total Months: " + str(monthscnt))
+print("Total: $" + str(int(total)))
+print("Average  Change: $" +str( "{0:.2f}".format(totalchange/(monthscnt-1))))
+print("Greatest Increase in Profits: " + str(max) + " ($" + str(increase) + ")")
+print("Greatest Decrease in Profits: " + str(min)+ " ($" + str(decrease) + ")")
+
