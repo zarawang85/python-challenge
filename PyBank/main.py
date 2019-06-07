60d7c655513d5bd8546fe8cd369161ed9501105e
zara <zara.wang85@gmail.com>
2019-06-07 13:54:17 -0400

    Comments added

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,36 +1,41 @@
 import os
 import csv
+# Specify the path of the file to read
 csvpath = os.path.join('dudget_data.csv')
-
+# Open the file using "read" mode.
 with open(csvpath, newline='') as csvfile:
-
     # CSV reader specifies delimiter and variable that holds contents
     budget = csv.reader(csvfile, delimiter=',')
+    # Read the header row first
     csv_header = next(budget)
-    # print(f"CSV Header: {csv_header}")
-
-    # Read each row of data after the header
-
-    monthscnt = 0
-    total = 0
-    profit_loss= []
-    month = []
-    change = []
-    max = ""
-    min = ""
-    totalchange = 0
        
+    monthscnt = 0     # Create a wordlist called monthscnt to hold No. of months
+    total = 0         # Create a integer called total to hold total profit
+    profit_loss= []   # Create a wordlist to hold profit of each months
+    month = []        # Create a wordlist called month to hold name of months
+    change = []       # Create a wordlist to hold the profit change between 2 months
+    max = ""          # Create a string to hold name of month with greatest increase
+    min = ""          # Create a string to hold name of month with greatest decrease
+    totalchange = 0   # Create a variable hold total change
+    
+    # Read each row of dataset after the header   
     for row in budget:
-        monthscnt = monthscnt +1
-        total = total + int(row[1])
-        month.append(row[0])
-        profit_loss.append(row[1])
+        monthscnt = monthscnt +1    # Add 1 to monthscnt in each iteration
+        total = total + int(row[1]) # Add profit to total in each iteration
+        month.append(row[0])        # Add name of month to wordlist "month" in each iteration
+        profit_loss.append(row[1])  # Add profit to wordlist "profit_loss" in each iteration
+    
+    # Remove first month from wordlist month, cuz the change cannot be calculated
     month.pop(0)
+    # Add the change in profit between 2 continuous months to wordlist "change"
+    # So change and month have same number of elements
     for i in range(len(profit_loss)-1):
         change.append(int(profit_loss[i+1])-int(profit_loss[i]))
    
-    increase = change[0]
-    decrease = change[0]
+    increase = change[0] # Give increase the value of the profit change between 1st and 2nd month
+    decrease = change[0] # Give decrease the value of the profit change between 1st and 2nd month
+    # Go through month and change, and save the value to increase whenever a larger change appears
+    # ,and save the value to decrease whenever a smaller change appears, and same the name of corresponding month to max and min
     for mon,cha in zip(month,change):
         if cha >= increase:
            increase = cha
@@ -38,18 +43,30 @@ with open(csvpath, newline='') as csvfile:
         if cha <= decrease:
            decrease = cha
            min = mon
+        # In each iteration add the value of change to totalchange
         totalchange = totalchange + cha
+
+# Print the result to the terminal
 print("Financial Analysis")
 print("-------------------------------------------------------------------")
+# Print the total months of the dataset
 print("Total Months: " + str(monthscnt))
+# Print the total profit or loss of all the months
 print("Total: $" + str(int(total)))
+# Print the average change
 print("Average  Change: $" +str( "{0:.2f}".format(totalchange/(monthscnt-1))))
+# Print the greatest increase in profits among all the months
 print("Greatest Increase in Profits: " + str(max) + " ($" + str(increase) + ")")
+# Print the greatest decrease in profits among all the months
 print("Greatest Decrease in Profits: " + str(min)+ " ($" + str(decrease) + ")")
 
+# Specify the file to write the result to
 output_file = os.path.join("output_pybank.csv")
+# Open the file using "write" mode. Specify the variable to hold the contents
 with open(output_file, "w", newline="") as datafile:
+    # Initialize csv.writer
     writer = csv.writer(datafile)
+    # Write the result to "output_pybank.csv"
     writer.writerow(["Financial Analysis"])
     writer.writerow(["-------------------------------------------------------------------"])
     writer.writerow(["Total Months: " + str(monthscnt)])
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,61 +1,82 @@
 import os
 import csv
+# Specify the path of the file to read
 csvpath = os.path.join('election_data.csv')
-
+# Open the file using "read" mode.
 with open(csvpath, newline='') as csvfile:
-
     # CSV reader specifies delimiter and variable that holds contents
     election = csv.reader(csvfile, delimiter=',')
-    csv_header = next(election)
-    # print(f"CSV Header: {csv_header}")
+    # Read the header row first
+    csv_header = next(election)    
 
-    # Read each row of data after the header
-
-    totalvote = 0
-    candidate= []
-    candidate_count = []
-      
+    totalvote = 0  # Create a variable to add up total number of votes
+    candidate= []  # Create a wordlist to hold candidates' names
+    candidate_count = [] # Create a wordlist to hold no. of candidates' votes
+    #count = 0
+    
+    # Read each row of data after the header  
     for row in election:
-        totalvote = totalvote +1
-        if row[2] not in candidate:
-            candidate.append(row[2])
+        # Add 1 to totalvote in each iteration
+        totalvote = totalvote +1    
+        # Whenever a new name appears add it to the candidate
+        if row[2] not in candidate: 
+            candidate.append(row[2]) 
      
-    count = 0
-    candidate_count = []
+    # Put zeros in candidate_count, of which the number is the same as that of candidate
     for person in candidate:
         candidate_count.append(0)
+    
+    # Open the dataset file again using "read" mode.
     with open(csvpath, newline='') as csvfile:
         election = csv.reader(csvfile, delimiter=',')
         csv_header = next(election)
+        # Read each row of data after the header
         for row in election:
+            # Compare each name in candidate with the name in each row
+            # Add 1 to corresponding elements in candidate_count
             for i in range(len(candidate)):
                 if row[2] == candidate[i]:
                    candidate_count[i] = candidate_count[i] + 1
     
-    result = []
-    percentage = ["{0:.3f}".format(number/totalvote*100) + "%" for number in candidate_count]
+    # Print the totoal votes, and statistic data of each candidate in terminal
     print("Election Results")
     print("-------------------------------------------------------------------")
     print("Total Votes: " + str(totalvote))
     print("-------------------------------------------------------------------")
+    
+    # Create a wordlist to hold candidates' statistic data
+    result = [] 
+    # Compute the % of votes of esch candidate and save it in wordlist percentage
+    percentage = ["{0:.3f}".format(number/totalvote*100) + "%" for number in candidate_count]    
+    # Creat "list" combine each element of candidate, percentage, and candidate_count
     list = zip(candidate,percentage,candidate_count)
+    # Go through the list and print each line to terminal, and save each line as element in result
     for j in list:
         print(j[0] + ": " + j[1] + " (" +str(j[2]) + ")")
         result.append(j[0] + ": " + j[1] + " (" +str(j[2]) + ")")
     print("-------------------------------------------------------------------")
-    largest = 0
-    winner = []
+    
+    largest = 0 # Create a variable to hold the number of votes of winnter
+    winner = [] # Create a wordlist to hold the data of winner
+    # Creat "list" combine each element of candidate, percentage, and candidate_count
     list = zip(candidate,percentage,candidate_count)
+    # Go through the list,if votes is larger than variable "largest" then update the "largest" with is no. of votes
+    # and save this row to winner
     for j in list:
         if j[2] >= largest:
            largest = j[2]
            winner = j
+    # Print winner's name
     print("Winner: " + winner[0])
     print("-------------------------------------------------------------------")
 
+# Specify the file to write the result to
 output_file = os.path.join("output_pypoll.csv")
+# Open the file using "write" mode. Specify the variable to hold the contents
 with open(output_file, "w", newline="") as datafile:
+    # Initialize csv.writer
     writer = csv.writer(datafile)
+    # Write the result line by line to ouput file
     writer.writerow(["Election Results"])
     writer.writerow(["-------------------------------------------------------------------"])
     writer.writerow(["Total Votes: " + str(totalvote)])
